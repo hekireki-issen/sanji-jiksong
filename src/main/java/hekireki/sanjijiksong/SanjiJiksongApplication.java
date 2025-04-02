91606ba457d807b881a4ef88959018f3a1583b56
Isonade2 <c1004sos@naver.com>
2025-04-02 15:47:14 +0900

    init: 프로젝트 생성 및 이슈, PR 템플릿 추가

---
+++ src/main/java/hekireki/sanjijiksong/SanjiJiksongApplication.java
@@ -0,0 +1,13 @@
+package hekireki.sanjijiksong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjiJiksongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanjiJiksongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hekireki/sanjijiksong/SanjiJiksongApplicationTests.java
@@ -0,0 +1,13 @@
+package hekireki.sanjijiksong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanjiJiksongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
