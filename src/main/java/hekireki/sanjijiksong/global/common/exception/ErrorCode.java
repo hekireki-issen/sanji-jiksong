e64b4bfc56b3faa034806107c650c0e06b40b126
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-04-04 17:20:14 +0900

    feat : 회원 복구, 비밀번호 재설정 추가

--- src/main/java/hekireki/sanjijiksong/global/common/exception/ErrorCode.java
+++ src/main/java/hekireki/sanjijiksong/global/common/exception/ErrorCode.java
@@ -13,6 +13,9 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
     USER_EMAIL_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
     USER_ALREADY_DEACTIVATED(HttpStatus.BAD_REQUEST, "이미 탈퇴된 사용자입니다."),
+    USER_ALREADY_RESTORED(HttpStatus.BAD_REQUEST, "이미 복구된 사용자입니다."),
+    USER_RESTORE_EXPIRED(HttpStatus.BAD_REQUEST, "복구 가능 기간이 만료되었습니다."),
+    USER_UNAUTHORIZED(HttpStatus.FORBIDDEN, "본인만 할 수 있습니다."),
 
     //Order
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 주문입니다.");
