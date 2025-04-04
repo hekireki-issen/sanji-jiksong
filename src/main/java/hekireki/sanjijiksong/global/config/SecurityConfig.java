56ee0732a9dad110f26e718f990dd894fb7db1d0
ahn_h_j <tkgkd246@gmail.com>
2025-04-05 00:39:42 +0900

    feat : SecurityConfig 추가
    disable : csrf, formLogin, httpBasic
    
    permitAll : /login, / , /join
    
    session : stateless
    
    LoginFilter 추가

---
+++ src/main/java/hekireki/sanjijiksong/global/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package hekireki.sanjijiksong.global.config;
+
+import hekireki.sanjijiksong.global.security.jwt.JwtUtil;
+import hekireki.sanjijiksong.global.security.jwt.LoginFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();//비밀번호 암호화
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf((auth) -> auth.disable());//jwt는 session을 stateless로 관리하기에 필요 없음
+        http.formLogin((auth)->auth.disable());//rest 방식이기에 login form 필요없음
+        http.httpBasic((auth) -> auth.disable());//시큐리티의 기본 인증인 HTTP Basic 인증 비활성화
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/login", "/", "/join").permitAll()//해당 url경로는 인증 필요 없음
+        );
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));//jwt는 session을 stateless로 관리
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);//로그인 필터 설정
+        return http.build();
+    }
+}
