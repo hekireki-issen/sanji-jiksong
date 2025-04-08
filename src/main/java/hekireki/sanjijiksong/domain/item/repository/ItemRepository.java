aad5f0eb8e7a5f46b8d54e4b179339cd5eb89743
ahn_h_j <tkgkd246@gmail.com>
2025-04-08 21:00:26 +0900

    feat : 내 제품 조회, 제품 상세 조회 기능 추가
    ItemController
    getMyItems()
    - 엔드포인트 : api/v1/stores/{storeId}/items
    - Role이 Seller인 사용자만 접근 가능
    getItemDetail()
    - 엔드포인트 : api/v1/stores/{storeId}/items/{itemId}
    
    ItemService
    getMyItems()
    - email을 통해 user 조회
    - 해당 user의 store 조회
    - store id 일치 여부 체크
    - item list 조회
    
    getItemDetail
    - storeId, itemId를 통해 item 조회

--- src/main/java/hekireki/sanjijiksong/domain/item/repository/ItemRepository.java
+++ src/main/java/hekireki/sanjijiksong/domain/item/repository/ItemRepository.java
@@ -3,5 +3,11 @@ package hekireki.sanjijiksong.domain.item.repository;
 import hekireki.sanjijiksong.domain.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findAllByStoreId(Long storeId);
+
+    Optional<Item> findByStoreIdAndId(Long storeId, Long itemId);
 }
--- src/main/java/hekireki/sanjijiksong/domain/item/service/ItemService.java
+++ src/main/java/hekireki/sanjijiksong/domain/item/service/ItemService.java
@@ -2,16 +2,23 @@ package hekireki.sanjijiksong.domain.item.service;
 
 import hekireki.sanjijiksong.domain.item.dto.ItemCreateRequest;
 import hekireki.sanjijiksong.domain.item.dto.ItemResponse;
+import hekireki.sanjijiksong.domain.item.dto.ItemUpdateRequest;
 import hekireki.sanjijiksong.domain.item.entity.Item;
 import hekireki.sanjijiksong.domain.item.repository.ItemRepository;
 import hekireki.sanjijiksong.domain.store.entity.Store;
+import hekireki.sanjijiksong.domain.store.repository.StoreRepository;
 import hekireki.sanjijiksong.domain.user.entity.User;
 import hekireki.sanjijiksong.domain.user.repository.UserRepository;
 import hekireki.sanjijiksong.global.common.exception.ErrorCode;
+import hekireki.sanjijiksong.global.common.exception.ItemException;
 import hekireki.sanjijiksong.global.common.exception.StoreException;
 import hekireki.sanjijiksong.global.common.exception.UserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +26,7 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
 
     public ItemResponse createItem(Long storeId, ItemCreateRequest itemCreateRequest, String email) {
         User user = userRepository.findByEmail(email)
@@ -35,6 +43,34 @@ public class ItemService {
         }
 
         Item savedItem = itemRepository.save(itemCreateRequest.toEntity(userStore));
-        return ItemResponse.of(savedItem);
+        return ItemResponse.from(savedItem);
     }
+
+    public List<ItemResponse> getMyItems(Long storeId, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
+
+        Store userStore = user.getStore();
+
+        if(userStore == null){
+            throw new StoreException(ErrorCode.STORE_NOT_FOUND);
+        }
+
+        if(!userStore.getId().equals(storeId)){
+            throw new StoreException(ErrorCode.UNAUTHORIZED_STORE_OWNER);
+        }
+
+        List<Item> AllItem = itemRepository.findAllByStoreId(storeId);
+        return AllItem.stream()
+                .map(ItemResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    public ItemResponse getItemDetail(Long storeId, Long itemId) {
+        Item item = itemRepository.findByStoreIdAndId(storeId, itemId)
+                .orElseThrow(() -> new ItemException(ErrorCode.ITEM_NOT_FOUND));
+        return ItemResponse.from(item);
+    }
+
+
 }
