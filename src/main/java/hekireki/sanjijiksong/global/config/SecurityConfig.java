6b8028ec623bfeb8b43fa631d6a73335b4db15b4
ahn_h_j <tkgkd246@gmail.com>
2025-04-08 13:29:33 +0900

    feat : PreAuthorize 적용을 위해 EnableMethodSecurity 어노테이션 추가

--- src/main/java/hekireki/sanjijiksong/global/config/SecurityConfig.java
+++ src/main/java/hekireki/sanjijiksong/global/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
