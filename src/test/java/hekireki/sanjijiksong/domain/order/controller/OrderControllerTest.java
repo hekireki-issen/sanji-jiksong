52f8419723a88e4db9b89db9e1ac12ca9630b893
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-04-11 14:37:09 +0900

    fix : import 제거

--- src/test/java/hekireki/sanjijiksong/domain/order/controller/OrderControllerTest.java
+++ src/test/java/hekireki/sanjijiksong/domain/order/controller/OrderControllerTest.java
@@ -22,7 +22,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
